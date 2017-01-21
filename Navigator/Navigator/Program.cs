b4f7881455afcef8deab7f39f70e46d901fa5de3
quantumbiology@hotmail.co.uk <quantumbiology@hotmail.co.uk>
2017-01-21 18:57:36 +0000

    nav

---
+++ Navigator/Navigator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Navigator
+{
+    public class Navigator
+    {
+        readonly String rootPath = @"C:\Sets\";
+        readonly String schoolPath = @"C:\Sets\Schools\";
+        readonly String studentPath = @"C:\Sets\Students\";
+
+        public String[] ShowStudents()
+        {
+            return Directory.GetFiles(studentPath);
+        }
+
+        public String[] ShowSchools()
+        {
+            return Directory.GetFiles(schoolPath);
+        }
+
+        public String GetStudentFilePath()
+        {
+            return studentPath;
+        }
+
+        public String GetSchoolFilePath()
+        {
+            return schoolPath;
+        }
+
+    }
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            Navigator n = new Navigator();
+            foreach (string s in n.ShowStudents())
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
