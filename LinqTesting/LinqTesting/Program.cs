81ecbf722a19d9c5fa07e0b5c90e3a761ecc0bf2
quantumbiology@hotmail.co.uk <quantumbiology@hotmail.co.uk>
2017-01-29 13:34:15 +0000

    ADO.NET stuff

---
+++ LinqTesting/LinqTesting/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using System.Data.OracleClient;
+using System.Data.Common;
+using System.Data;
+
+namespace LinqTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DbProviderFactory dbf = DbProviderFactories.GetFactory("MySql.Data.MySqlClient");
+                using (DbConnection dbcn = dbf.CreateConnection())
+                {
+                    dbcn.ConnectionString = "Server=localhost;Database=schools;Uid=root;Pwd=0x38be;";
+                    dbcn.Open();
+                    using (DbCommand dbcmd = dbcn.CreateCommand())
+                    {
+                        dbcmd.CommandType = CommandType.Text;
+                        dbcmd.CommandText = "SHOW TABLES;";
+                        using (DbDataReader dbrdr = dbcmd.ExecuteReader())
+                        {
+                            while (dbrdr.Read())
+                            {
+                                Console.WriteLine(dbrdr[0]);
+                            }
+                        }
+                    }
+                }
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
