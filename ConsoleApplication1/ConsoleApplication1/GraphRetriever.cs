48e311fc6f2562140564196c92635cb793420ef8
fossildog117 <nathanliu@XaEs.local>
2017-03-22 00:30:56 +0000

    added GraphRetriever Class

---
+++ ConsoleApplication1/ConsoleApplication1/GraphRetriever.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace CE
+{
+	public static class GraphRetriever
+	{
+
+		static ChartEngine engine = new ChartEngine();
+
+		public static BarChart GetBarChart(string request, string title)
+		{
+
+			engine.chart = new BarChart(title, "Frequency");
+
+			// request = C:/Sets/Students/STU$gcse$A/ for now
+			engine.calculateHeights(request);
+
+			return (BarChart)engine.chart;
+		}
+
+		public static PieChart GetPieChart(string request)
+		{
+			engine.chart = new PieChart();
+
+			// request = C:/Sets/Students/STU$gcse$A/
+			engine.calculateHeights(request);
+
+			return (PieChart)engine.chart;
+		}
+
+		public static LineGraph GetLineGraph(string request, string xAxis = "", string yAxis = "", string title = "")
+		{
+
+			engine.chart = new LineGraph(xAxis, yAxis, title);
+
+			// request = C:/Sets/Students/STU$gcse$A/
+			engine.calculateHeights(request);
+
+			return (LineGraph)engine.chart;
+		}
+
+	}
+}
+
--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,19 +10,36 @@ namespace CE
 
     class Program
     {
+
         static void Main(string[] args)
         {
-            ChartEngine engine = new ChartEngine();
 
-            engine.chart = new BarChart("Number of As", "Frequency");
-            engine.calculateHeights("C:/Sets/Students/STU$gcse$A/");
+			LineGraph bChart = GraphRetriever.GetLineGraph("C:/Sets/Students/STU$gcse$B/");
 
-            foreach (Bar b in engine.chart.bars)
-            {
+			foreach (Bar b in bChart.bars) {
                 Console.WriteLine(b.uri + " : " + b.height);
             }
 
-            Console.WriteLine("Done");
+            Console.WriteLine("Retrieved Line Graph");
+			Console.WriteLine("-------------------");
+
+			BarChart barChart = GraphRetriever.GetBarChart("C:/Sets/Students/STU$gcse$A/", "Number of As");
+
+			foreach (Bar b in barChart.bars) {
+				Console.WriteLine(b.uri + " : " + b.height);
+			}
+
+			Console.WriteLine("Retrieved Bar Chart");
+			Console.WriteLine("-------------------");
+
+			PieChart pieChart = GraphRetriever.GetPieChart("C:/Sets/Students/STU$gcse$C/");
+
+			foreach (Bar b in pieChart.bars) {
+				Console.WriteLine(b.uri + " : " + b.height);
+			}
+
+			Console.WriteLine("Retrieved Pie Chart");
+			Console.WriteLine("-------------------");
 
             Console.ReadKey();
         }
