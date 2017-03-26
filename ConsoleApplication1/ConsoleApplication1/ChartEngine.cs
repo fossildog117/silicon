e0c477667a693d236b97311575b3f8db6934dc69
fossildog117 <nathanliu@XaEs.local>
2017-03-26 00:08:14 +0000

    Update ChartEngine and Blob Reader to read info set

--- ConsoleApplication1/ConsoleApplication1/ChartEngine.cs
+++ ConsoleApplication1/ConsoleApplication1/ChartEngine.cs
@@ -11,98 +11,6 @@ using System.Text.RegularExpressions;
 
 namespace CE
 {
-    public class BlobReader
-    {
-
-        public string blobConnString { get; }
-        public CloudStorageAccount storageAccount { get; }
-        public CloudBlobClient blobClient { get; }
-        public CloudBlobContainer blobContainer { get; }
-
-        public BlobReader()
-        {
-            this.blobConnString = ConfigurationManager.ConnectionStrings["azureStorageConnection"].ConnectionString;
-            this.storageAccount = CloudStorageAccount.Parse(blobConnString);
-            this.blobClient = storageAccount.CreateCloudBlobClient();
-            this.blobContainer = blobClient.GetContainerReference("sets");
-        }
-
-        public byte[] DownloadData(string request)
-        {
-			byte[] arrayOutput;
-			using (MemoryStream memoryStream = new MemoryStream())
-			{
-				try
-				{
-					blobContainer.GetBlockBlobReference(request).DownloadToStream(memoryStream);
-				}
-				catch
-				{
-					Console.WriteLine("Failed to download data from blob.");
-				}
-				finally 
-				{
-					memoryStream.Position = 0;
-					arrayOutput = memoryStream.ToArray();
-					memoryStream.Flush();
-				}
-			}
-
-			return arrayOutput;
-
-        }
-
-		public string GetJson(string request)
-        {
-			string v = Encoding.UTF8.GetString(DownloadData(request));
-			string s = Regex.Replace(v, ".*{", "{");
-			string t = Regex.Replace(s, "}.*", "}");
-			return t;
-		}
-
-        public int GetSetSize(string request)
-        {          
-			int length = GetJson(request).Split(',').Length;
-            return length > 0 ? length - 1 : 0;
-        }
-
-        public Dictionary<int, string> GetSet(string request)
-        {
-            Dictionary<int, string> blobData = new Dictionary<int, string>();
-
-			string data = Encoding.UTF8.GetString(DownloadData(request));
-            string[] values = data.Split(',');
-
-            for (int i = 1; i < values.Length; i++)
-            {
-                string s = Regex.Replace(values[i], "\"|.*{|}.*", "");
-                string[] pair = s.Split(':');
-                blobData.Add(Int32.Parse(pair[0]), pair[1]);
-            }
-
-            return blobData;
-
-        }
-
-		public Dictionary<string, string> GetMap()
-		{
-			Dictionary<string, string> blobData = new Dictionary<string, string>();
-
-			string data = GetJson("SetMap");
-			string[] values = data.Split(',');
-
-			for (int i = 1; i < values.Length; i++)
-			{
-				string s = Regex.Replace(values[i], "\"|.*{|}.*", "");
-				string[] pair = s.Split(':');
-				blobData.Add(pair[0], pair[1]);
-			}
-
-			return blobData;
-
-		}
-
-    }
 
     public class ChartEngine
     {
@@ -138,11 +46,14 @@ namespace CE
             string setName = "sets";
             List<Bar> outputList = new List<Bar>();
 
-            foreach (var value in reader.blobContainer.ListBlobs(directory))
-            {
-                string filePath = value.Uri.LocalPath.Substring(setName.Length + 2);
-                outputList.Add(new Bar(value.Uri.LocalPath.Substring(setName.Length + 2), findValue(filePath)));
-            }
+			foreach (var value in reader.blobContainer.ListBlobs(directory))
+			{
+				string filePath = value.Uri.LocalPath.Substring(setName.Length + 2);
+				if (!filePath.Contains("-size"))
+				{
+					outputList.Add(new Bar(value.Uri.LocalPath.Substring(setName.Length + 2), findValue(filePath)));
+				}
+			}
 
             return outputList;
         }
@@ -154,10 +65,12 @@ namespace CE
             Task[] tasks = new Task[chart.bars.Count];
             int counter = 0;
 
-            foreach (Bar directory in chart.bars)
-            {
-                tasks[counter++] = Task.Factory.StartNew(() => setHeight(directory));
-            }
+			foreach (Bar directory in chart.bars)
+			{
+
+				tasks[counter++] = Task.Factory.StartNew(() => setHeight(directory));
+
+			}
 
             Task.WaitAll(tasks);
 
@@ -165,7 +78,7 @@ namespace CE
 
         private void setHeight(Bar bar)
         {
-            bar.height = reader.GetSetSize(bar.uri);
+			bar.height = reader.GetSetSize(bar.uri, true);
         }
     }
 }
--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -13,18 +13,15 @@ namespace CE
         static void Main(string[] args)
         {
 
-			//LineGraph bChart = GraphRetriever.GetLineGraph("C:/Sets/Students/STU$gcse$B/");
+			LineGraph bChart = GraphRetriever.GetLineGraph("Sets/School/school/city/");
 
-			//foreach (Bar b in bChart.bars) {
-			//             Console.WriteLine(b.uri + " : " + b.height);
-			//         }
+			foreach (Bar b in bChart.bars) {
+			             Console.WriteLine(b.uri + " : " + b.height);
+			         }
 
-			//         Console.WriteLine("Retrieved Line Graph");
-			//Console.WriteLine("-------------------");
+			         Console.WriteLine("Retrieved Line Graph");
+			Console.WriteLine("-------------------");
 
-			var map = GraphRetriever.GetMap();
-
-			Dictionary<int, string> mySet = GraphRetriever.GetSetWithKey("student_stats-attendance");
 
 			//Console.ReadKey();
         }
